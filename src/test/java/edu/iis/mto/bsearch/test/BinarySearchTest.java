6d090163213ac0124e889efc466defec19522572
Famulski.Michal <187684@edu.p.lodz.pl>
2018-08-23 15:03:15 +0200

    testy_v1 Added test when key is found in one sized sequence

---
+++ src/test/java/edu/iis/mto/bsearch/test/BinarySearchTest.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.bsearch.test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+
+public class BinarySearchTest {
+
+    @Test
+    public void elementFoundInSequenceWhenSequenceSizeEqualToOne() {
+        int key = 2;
+        int[] seq = {2};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertThat(result.isFound(), equalTo(true));
+    }
+
+}
